50b5361f5248a57153cddefd4ba3f02975e4698d
Irakli Tchitadze <iraklilion@gmail.com>
2017-08-23 13:05:22 -0400

    improvements and code clean up

--- PiOSDrone/PiOSDrone.ino
+++ PiOSDrone/PiOSDrone.ino
@@ -1,42 +1,24 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <WiFiClient.h>
 #include <SPI.h>
 #include <WebSocketsServer.h>
 
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-#define USE_SERIAL Serial
-
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
     switch (type) {
         case WStype_DISCONNECTED:
-            USE_SERIAL.printf("[%u] Disconnected!\n", num);
+            Serial.printf("[%u] Disconnected!\n", num);
             break;
+
         case WStype_CONNECTED: {
             IPAddress ip = webSocket.remoteIP(num);
-            USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-
-            // send message to client
-            webSocket.sendTXT(num, "Connected");
+            Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+        	  break;
         }
-            break;
+
         case WStype_TEXT:
-            USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
-
-            // send message to client
-            // webSocket.sendTXT(num, "message here");
-
-            // send data to all connected clients
-            // webSocket.broadcastTXT("message here");
-            break;
-        case WStype_BIN:
-            USE_SERIAL.printf("[%u] get binary length: %u\n", num, length);
-            hexdump(payload, length);
-
-            // send message to client
-            // webSocket.sendBIN(num, payload, length);
+            Serial.printf("%s\n", payload);
             break;
     }
 }
@@ -48,26 +30,19 @@ void setup() {
     delay(1000);
     Serial.begin(115200);
     Serial.println();
-    Serial.print("Configuring access point...");
-    /* You can remove the password parameter if you want the AP to be open. */
+    Serial.println("Configuring access point...");
+   
     WiFi.softAP(ssid, password);
 
     IPAddress myIP = WiFi.softAPIP();
+    
     Serial.print("AP IP address: ");
     Serial.println(myIP);
-
-    USE_SERIAL.setDebugOutput(true);
-
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-
-    for (uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-    }
-
+    Serial.print("AP SSID: ");
+    Serial.println(ssid);
+    Serial.print("AP PASS: ");
+    Serial.println(password);
+    
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
 }
